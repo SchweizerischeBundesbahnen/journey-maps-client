135ba6c366cfa01cdf5b15d8fe7159a56d89cce5
ue68740 <sebastian.steiner@sbb.ch>
2020-09-30 11:34:27 +0200

    ses, DUSP-1056 Run tests with Puppeteer.

--- projects/journey-maps-client-testapp/src/app/app.component.spec.ts
+++ projects/journey-maps-client-testapp/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import {TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {JourneyMapsClientComponent} from 'journey-maps-client';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        JourneyMapsClientComponent
       ],
     }).compileComponents();
   });
