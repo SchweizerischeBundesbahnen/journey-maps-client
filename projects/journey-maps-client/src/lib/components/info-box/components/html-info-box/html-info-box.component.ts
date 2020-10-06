861e6284cd0c08a21288880dd2888c23c7370cd2
ue68740 <sebastian.steiner@sbb.ch>
2020-10-06 11:25:46 +0200

    ses, DUSP-1056 InfoBlocks implemented.

---
+++ projects/journey-maps-client/src/lib/components/info-box/components/html-info-box/html-info-box.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {HtmlInfoBlock} from '../../../../model/infoblock/html-info-block';
+import {DomSanitizer} from '@angular/platform-browser';
+
+@Component({
+  selector: 'rokas-html-info-box',
+  templateUrl: './html-info-box.component.html',
+  styleUrls: ['./html-info-box.component.scss']
+})
+export class HtmlInfoBoxComponent implements OnInit {
+
+  private _infoBlock: HtmlInfoBlock;
+  html: any;
+
+  constructor(private sanitized: DomSanitizer) {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  get infoBlock(): HtmlInfoBlock {
+    return this._infoBlock;
+  }
+
+  @Input() set infoBlock(value: HtmlInfoBlock) {
+    this._infoBlock = value;
+    this.html = this.sanitized.bypassSecurityTrustHtml(value.html);
+  }
+}
