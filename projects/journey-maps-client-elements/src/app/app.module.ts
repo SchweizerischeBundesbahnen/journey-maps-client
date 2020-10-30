e47b7066136dd5721937e9ecc5e6a5a52ddea392
ue68740 <sebastian.steiner@sbb.ch>
2020-10-30 07:48:12 +0100

    ses, Delete default component.

--- projects/journey-maps-client-elements/src/app/app.module.ts
+++ projects/journey-maps-client-elements/src/app/app.module.ts
@@ -1,17 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule {
 }
