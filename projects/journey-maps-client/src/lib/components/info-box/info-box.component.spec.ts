6efca5020d24c3c66fa6aa99c50a32989f860072
ue70941 <christopher.dickinson@sbb.ch>
2021-02-11 19:16:39 +0200

    cdi, DUSP-1275 some initial attempts

---
+++ projects/journey-maps-client/src/lib/components/info-box/info-box.component.spec.ts
@@ -0,0 +1,57 @@
+import {InfoBoxComponent} from './info-box.component';
+import {InfoBlockType} from '../../model/infoblock/info-block-type.enum';
+import {TextInfoBlock} from '../../model/infoblock/text-info-block';
+
+describe('InfoBoxComponent#shouldRender', () => {
+  const cut = new InfoBoxComponent(null);
+
+  it('should return false if !selectedMarker', () => {
+    [null, undefined, null].forEach(selectedMarker => {
+      cut.selectedMarker = selectedMarker;
+      expect(cut.shouldRender()).toBeFalse();
+    });
+  });
+
+  it('should return false if !selectedMarker.infoBlocks and !infoBoxTemplate', () => {
+    cut.selectedMarker = {...createMarkerWithoutInfoBlocks()};
+    expect(cut.shouldRender()).toBeFalse();
+  });
+
+  it('should return false if !selectedMarker.infoBlocks.length and !infoBoxTemplate', () => {
+    cut.selectedMarker = {
+      ...createMarkerWithoutInfoBlocks(),
+      infoBlocks: [],
+    };
+    expect(cut.shouldRender()).toBeFalse();
+  });
+
+  it('should return true if infoBoxTemplate', () => {
+    // @ts-ignore
+    cut.selectedMarker = createMarkerWithoutInfoBlocks();
+    // TODO
+    // cut.infoBoxTemplate = {};
+    expect(cut.shouldRender()).toBeTrue();
+  });
+
+  it('should return true if selectedMarker.infoBlocks.length', () => {
+    // @ts-ignore
+    cut.selectedMarker = {
+      infoBlocks: [
+        {
+          type: InfoBlockType.TEXT,
+          title: 'Lorem Ipsum',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit',
+          cssClass: undefined
+        } as TextInfoBlock
+      ]
+    };
+    expect(cut.shouldRender()).toBeTrue();
+  });
+});
+
+const createMarkerWithoutInfoBlocks = () => ({
+  id: 'work',
+  title: 'Office',
+  position: [7.446450, 46.961409],
+  category: 'MISSED_CONNECTION',
+});
