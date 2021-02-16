f8f7ed8179be8d69dd452c50acfbfd77ac29d35d
ue70941 <christopher.dickinson@sbb.ch>
2021-02-16 16:19:04 +0200

    cdi, DUSP-1275 testing the InfoBoxComponent

--- projects/journey-maps-client/src/lib/components/info-box/info-box.component.spec.ts
+++ projects/journey-maps-client/src/lib/components/info-box/info-box.component.spec.ts
@@ -1,56 +1,99 @@
 import {InfoBoxComponent} from './info-box.component';
-import {InfoBlockType} from '../../model/infoblock/info-block-type.enum';
 import {TextInfoBlock} from '../../model/infoblock/text-info-block';
 import {TemplateRef} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ButtonInfoBlock} from '../../model/infoblock/button-info-block';
+import {SbbIcon} from '@sbb-esta/angular-core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TextInfoBoxComponent} from './components/text-info-box/text-info-box.component';
+import {ButtonInfoBoxComponent} from './components/button-info-box/button-info-box.component';
+import {SbbIconTestingModule} from '@sbb-esta/angular-core/icon/testing';
+import {By} from '@angular/platform-browser';
 
-describe('InfoBoxComponent#shouldRender', () => {
-  const cut = new InfoBoxComponent(null);
+describe('InfoBoxComponent', () => {
+  let component: InfoBoxComponent;
+  let fixture: ComponentFixture<InfoBoxComponent>;
+  let closeClicked: boolean;
 
   beforeEach(() => {
-    cut.infoBoxTemplate = undefined;
+    TestBed.configureTestingModule({
+      declarations: [
+        InfoBoxComponent,
+        SbbIcon,
+        TextInfoBoxComponent,
+        ButtonInfoBoxComponent,
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        SbbIconTestingModule,
+      ],
+    });
+    fixture = TestBed.createComponent(InfoBoxComponent);
+    component = fixture.componentInstance;
+    closeClicked = false;
+    expect(getCloseButtonDe()).toBeNull();
+  });
+
+  const prepareSelectedMarker = () => {
+    component.closeClicked.subscribe(() => closeClicked = true);
+    component.selectedMarker = createMarkerWithInfoBlocks();
+    fixture.detectChanges();
+    expect(closeClicked).toBeFalse();
+    expect(getCloseButtonDe()).not.toBeNull();
+  };
+
+  const getCloseButtonDe = () => fixture.debugElement.query(By.css('[data-testid="close-button"]'));
+
+  it('should emit closeClicked event if close button is clicked', () => {
+    prepareSelectedMarker();
+    getCloseButtonDe().nativeElement.click();
+    expect(closeClicked).toBeTrue();
+  });
+
+  it('should emit closeClicked event if escape key is clicked', () => {
+    prepareSelectedMarker();
+    component.onEscapePressed();
+    expect(closeClicked).toBeTrue();
+  });
+});
+
+describe('InfoBoxComponent#shouldRender', () => {
+  const component = new InfoBoxComponent(null);
+
+  beforeEach(() => {
+    component.infoBoxTemplate = undefined;
   });
 
   it('should return false if !selectedMarker', () => {
     [null, undefined, null].forEach(selectedMarker => {
-      cut.selectedMarker = selectedMarker;
-      expect(cut.shouldRender()).toBeFalse();
+      component.selectedMarker = selectedMarker;
+      expect(component.shouldRender()).toBeFalse();
     });
   });
 
   it('should return false if !selectedMarker.infoBlocks and !infoBoxTemplate', () => {
-    cut.selectedMarker = {...createMarkerWithoutInfoBlocks()};
-    expect(cut.shouldRender()).toBeFalse();
+    component.selectedMarker = {...createMarkerWithoutInfoBlocks()};
+    expect(component.shouldRender()).toBeFalse();
   });
 
   it('should return false if !selectedMarker.infoBlocks.length and !infoBoxTemplate', () => {
-    cut.selectedMarker = {
+    component.selectedMarker = {
       ...createMarkerWithoutInfoBlocks(),
       infoBlocks: [],
     };
-    expect(cut.shouldRender()).toBeFalse();
+    expect(component.shouldRender()).toBeFalse();
   });
 
   it('should return true if infoBoxTemplate', () => {
-    // @ts-ignore
-    cut.selectedMarker = createMarkerWithoutInfoBlocks();
+    component.selectedMarker = createMarkerWithoutInfoBlocks();
     // dirty hack
-    cut.infoBoxTemplate = 1 as unknown as TemplateRef<any>;
-    expect(cut.shouldRender()).toBeTrue();
+    component.infoBoxTemplate = 1 as unknown as TemplateRef<any>;
+    expect(component.shouldRender()).toBeTrue();
   });
 
   it('should return true if selectedMarker.infoBlocks.length', () => {
-    // @ts-ignore
-    cut.selectedMarker = {
-      infoBlocks: [
-        {
-          type: InfoBlockType.TEXT,
-          title: 'Lorem Ipsum',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit',
-          cssClass: undefined
-        } as TextInfoBlock
-      ]
-    };
-    expect(cut.shouldRender()).toBeTrue();
+    component.selectedMarker = createMarkerWithInfoBlocks();
+    expect(component.shouldRender()).toBeTrue();
   });
 });
 
@@ -59,4 +102,20 @@ const createMarkerWithoutInfoBlocks = () => ({
   title: 'Office',
   position: [7.446450, 46.961409],
   category: 'MISSED_CONNECTION',
+});
+
+const createMarkerWithInfoBlocks = () => ({
+  ...createMarkerWithoutInfoBlocks(),
+  infoBlocks: [
+    {
+      type: 'TEXT',
+      title: 'Lorem Ipsum',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisici elit',
+    } as TextInfoBlock,
+    {
+      type: 'BUTTON',
+      title: 'Bavaria Ipsum',
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    } as ButtonInfoBlock,
+  ],
 });
