8c5994fbb679222aaac7147d384e75e9a30b44d9
ue70941 <christopher.dickinson@sbb.ch>
2021-02-20 22:45:15 +0200

    cdi, DUSP-1275 adding more tests and refactoring

--- projects/journey-maps-client/src/lib/services/multiTouchSupport.spec.ts
+++ projects/journey-maps-client/src/lib/services/multiTouchSupport.spec.ts
@@ -76,6 +76,42 @@ describe('MultiTouchSupport', () => {
     expectSetZoomToHaveBeenCalledWithNearly(11.132, 3);
   });
 
+  it('pans and zooms correctly in multiple steps', () => {
+    // given
+    const startEvent = {touches: [{screenX: 2, screenY: 2}, {screenX: 3, screenY: 3}]};
+    const moveEvent1 = {touches: [{screenX: 2.01, screenY: 2.01}, {screenX: 3.01, screenY: 3.01}]};
+    const moveEvent2 = {touches: [{screenX: 1.99, screenY: 1.99}, {screenX: 3.01, screenY: 3.01}]};
+
+    // when
+    service.touchStart(startEvent);
+    service.touchMove(moveEvent1);
+
+    // then
+    expectPanByToHaveBeenCalledWithNearly([-0.01, -0.01], 2);
+    expect(map.setZoom).toHaveBeenCalledOnceWith(initialZoom);
+
+    // when
+    service.touchMove(moveEvent2);
+
+    // then
+    expectPanByToHaveBeenCalledWithNearly([0.01, 0.01], 2); // the average of finger 1 and finger 2 moves
+    expectSetZoomToHaveBeenCalledWithNearly(11.132, 3);
+  });
+
+  it('doesn\'t pan or zoom if fingers only rotate', () => {
+    // given
+    const startEvent = {touches: [{screenX: 2.00, screenY: 2.00}, {screenX: 3.00, screenY: 3.00}]};
+    const moveEvent = {touches: [{screenX: 1.99, screenY: 2.01}, {screenX: 3.01, screenY: 2.99}]};
+
+    // when
+    service.touchStart(startEvent);
+    service.touchMove(moveEvent);
+
+    // then
+    expect(map.panBy).toHaveBeenCalledOnceWith([-0, -0], jasmine.anything());
+    expect(map.setZoom).toHaveBeenCalledOnceWith(initialZoom);
+  });
+
   it('DOESN\'T zoom beneath the TOUCH_ZOOM_THRESHOLD', () => {
     // given
     const startEvent = {touches: [{screenX: 2, screenY: 2}, {screenX: 3, screenY: 3}]};
--- projects/journey-maps-client/src/lib/services/multiTouchSupport.ts
+++ projects/journey-maps-client/src/lib/services/multiTouchSupport.ts
@@ -23,7 +23,7 @@ export class MultiTouchSupport {
     let x = 0;
     let y = 0;
 
-    [].forEach.call(event.touches, (touch) => {
+    event.touches.forEach((touch) => {
       x += touch.screenX;
       y += touch.screenY;
     });
@@ -64,7 +64,7 @@ export class MultiTouchSupport {
     let x = 0;
     let y = 0;
 
-    [].forEach.call(event.touches, (touch) => {
+    event.touches.forEach((touch) => {
       x += touch.screenX;
       y += touch.screenY;
     });
@@ -75,7 +75,7 @@ export class MultiTouchSupport {
     this.state.panStart.x = x / event.touches.length;
     this.state.panStart.y = y / event.touches.length;
 
-    this.map.panBy([movex / -1, movey / -1], {animate: false});
+    this.map.panBy([-movex, -movey], {animate: false});
   }
 
   onAdd(map): HTMLDivElement {
