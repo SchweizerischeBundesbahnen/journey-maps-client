00095c6d29e7509cf0904eeb952b16975971dbee
U230430 <marek.stankala@sbb.ch>
2021-07-08 10:32:10 +0200

    DUSP-1462: LeitPois cleanup

--- projects/journey-maps-client/src/lib/components/leit-poi/model/leit-poi-placement.ts
+++ projects/journey-maps-client/src/lib/components/leit-poi/model/leit-poi-placement.ts
@@ -1,7 +1,7 @@
 export enum LeitPoiPlacement {
-  default = 0,
-  northeast = 1,
-  southeast = 2,
-  southwest = 3,
-  northwest = 4
+  default,
+  northeast,
+  southeast,
+  southwest,
+  northwest
 }
