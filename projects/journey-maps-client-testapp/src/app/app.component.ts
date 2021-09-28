110589a4a02045d5881f336aeafe88c43f450381
ue70941 <christopher.dickinson@sbb.ch>
2021-09-28 17:19:38 +0200

    cdi, ROKAS-302 revert commit 'edea3336'. We still need to fix the ExpressionChangedAfterItHasBeenCheckedError.

--- projects/journey-maps-client-testapp/src/app/app.component.ts
+++ projects/journey-maps-client-testapp/src/app/app.component.ts
@@ -37,7 +37,6 @@ export class AppComponent implements OnInit, OnDestroy {
   selectedMarkerId: string;
   visibleLevels: number[];
   selectedLevel: number;
-  selectedLevelOutput: number;
   boundingBox: LngLatBoundsLike = [[6.02260949059, 45.7769477403], [10.4427014502, 47.8308275417]];
   allowOneFingerPan = true;
   popup = true;
