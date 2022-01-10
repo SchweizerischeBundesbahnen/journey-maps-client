9f5683094f9410cee9e9fc2668a893a9b7f15cb7
U230430 <marek.stankala@sbb.ch>
2022-01-10 14:56:41 +0100

    ROKAS-494: reset j-m-c files

--- projects/journey-maps-client/src/lib/journey-maps-client.module.ts
+++ projects/journey-maps-client/src/lib/journey-maps-client.module.ts
@@ -7,21 +7,16 @@ import {AngularResizedEventModule} from 'angular-resize-event';
 import {GlobalStylesComponent} from './components/global-styles/global-styles.component';
 import {MarkerDetailsComponent} from './components/marker-details/marker-details.component';
 import {LevelSwitchComponent} from './components/level-switch/level-switch.component';
-import {
-  TextInfoBlockComponent
-} from './components/marker-details/components/info-block-renderer/components/text-info-block/text-info-block.component';
-import {
-  ButtonInfoBlockComponent
-} from './components/marker-details/components/info-block-renderer/components/button-info-block/button-info-block.component';
+import {TextInfoBlockComponent} from './components/marker-details/components/info-block-renderer/components/text-info-block/text-info-block.component';
+import {ButtonInfoBlockComponent} from './components/marker-details/components/info-block-renderer/components/button-info-block/button-info-block.component';
 import {PopupComponent} from './components/marker-details/components/popup/popup.component';
 import {TeaserComponent} from './components/marker-details/components/teaser/teaser.component';
 import {InfoBlockRendererComponent} from './components/marker-details/components/info-block-renderer/info-block-renderer.component';
-import {
-  AddressInfoBlockComponent
-} from './components/marker-details/components/info-block-renderer/components/address-info-block/address-info-block.component';
+import {AddressInfoBlockComponent} from './components/marker-details/components/info-block-renderer/components/address-info-block/address-info-block.component';
 import {ZoomControlsComponent} from './components/zoom-controls/zoom-controls.component';
 import {LeitPoiComponent} from './components/leit-poi/leit-poi.component';
 import {BasemapSwitchComponent} from './components/basemap-switch/basemap-switch.component';
+
 
 @NgModule({
   declarations: [
