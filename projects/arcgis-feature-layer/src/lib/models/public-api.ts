e2a248bdafeba0e1b951493f6cc68509d2bed7d7
U230430 <marek.stankala@sbb.ch>
2022-01-11 15:04:29 +0100

    ROKAS-494: public api

--- projects/arcgis-feature-layer/src/lib/models/public-api.ts
+++ projects/arcgis-feature-layer/src/lib/models/public-api.ts
@@ -2,12 +2,13 @@
  * Public API Surface of arcgis-feature-layer
  */
 
-export * from './feature-layer-options';
-export * from './feature-layer-config';
-export * from './arcgis-symbol-definition';
 export * from './any-feature-layer-renderer-info';
+export * from './arcgis-symbol-definition';
+export * from './feature-layer-config';
+export * from './feature-layer-error';
 export * from './feature-layer-heatmap-renderer-info';
+export * from './feature-layer-info-response';
+export * from './feature-layer-options';
 export * from './feature-layer-simple-renderer-info';
 export * from './feature-layer-unique-value-renderer-info';
-export * from './feature-layer-info-response';
-export * from './feature-layer-error';
+export * from './http-basic-auth-interceptor-config';
