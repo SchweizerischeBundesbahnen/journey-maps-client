8f18b348b48562b9742d178bab1191698e959ada
U230430 <marek.stankala@sbb.ch>
2022-01-17 15:32:29 +0100

    ROKAS-494: api documentation

--- projects/arcgis-feature-layer/src/lib/interceptors/http-basic-auth-interceptor-config.ts
+++ projects/arcgis-feature-layer/src/lib/interceptors/http-basic-auth-interceptor-config.ts
@@ -1,5 +1,8 @@
 import {InjectionToken} from '@angular/core';
 import {HttpBasicAuthInterceptorConfig} from '../models/http-basic-auth-interceptor-config';
 
-
+/**
+ * HttpBasicAuthInterceptor configuration including the service urls to force the basic auth popup on cross-origin requests.
+ * By default, 'geo.sbb.ch', 'geo-int.sbb.ch' and 'geo-dev.sbb.ch'.
+ */
 export const HTTP_BASIC_AUTH_INTERCEPTOR_CONFIG_TOKEN = new InjectionToken<HttpBasicAuthInterceptorConfig>('{}');
