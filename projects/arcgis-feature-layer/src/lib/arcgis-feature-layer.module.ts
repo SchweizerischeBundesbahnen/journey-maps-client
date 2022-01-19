67da1fd3b5a644bd204e1416d8c1db4bb8929ffe
U230430 <marek.stankala@sbb.ch>
2022-01-19 12:28:57 +0100

    ROKAS-494: cleanup

--- projects/arcgis-feature-layer/src/lib/arcgis-feature-layer.module.ts
+++ projects/arcgis-feature-layer/src/lib/arcgis-feature-layer.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ArcgisFeatureLayerComponent} from './arcgis-feature-layer.component';
-import {HTTP_BASIC_AUTH_INTERCEPTOR_CONFIG_TOKEN} from './interceptors/http-basic-auth-interceptor-config';
-import {HttpBasicAuthInterceptor} from './interceptors/http-basic-auth-interceptor';
 
 @NgModule({
   declarations: [ArcgisFeatureLayerComponent],
